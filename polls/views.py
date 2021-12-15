5779b580326975a8817031dfd398385df5089666
Robert Granja <robeertgr@live.com>
2021-12-15 16:18:16 -0300

    Criado a pagina de votação

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, response
 from .models import Question
 
@@ -13,4 +13,15 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('Você está votando na pergunta nº %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except KeyError:
+        return render(request, 'polls/vote.html', {
+            'question': question,
+            'error_message': "Você não selecionou uma resposta!",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
