173e3ae65678776117db36d6a993bab45c35f4cc
Robert Granja <robeertgr@live.com>
2021-12-15 15:53:34 -0300

    Criado o HTML

--- polls/views.py
+++ polls/views.py
@@ -6,13 +6,11 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return HttpResponse("Olá, este é o meu primeiro site")
-
-def detail(request, question_id):
-    return HttpResponse('Esta é a pergunta nº %s' % question_id)  # ou usar {} .format(%question_id)
+    return render(request, 'polls/index.html', context)
 
 def results(request, question_id):
-    response = ('Estes são os resultados da pergunta nº %s' % question_id)
+    question = Question(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse('Você está votando na pergunta nº %s' % question_id)
